9701d95947030a67ef59f227632d4e194ca66c3d
francois_popieul <francois.popieul@hotmail.fr>
2025-08-30 23:50:18 +0200

    fixed burger menu behaviour

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,23 @@
     factory();
 })((function () { 'use strict';
 
+    const body = document.querySelector("body");
     const burgerMenu = document.getElementById("burger-menu");
     const burgerMenuLinkContainer = document.getElementById("burger-menu-link-container");
+    if (burgerMenu) {
+        burgerMenu.addEventListener("click", (e) => {
+            e.stopPropagation();
+        });
+    }
+    if (body && burgerMenuLinkContainer) {
+        body.addEventListener("click", () => {
+            if (!burgerMenuLinkContainer.classList.contains("hidden")) {
+                burgerMenuLinkContainer.classList.toggle("hidden");
+            }
+        });
+    }
     if (burgerMenu && burgerMenuLinkContainer) {
         burgerMenu.addEventListener("click", () => {
-            burgerMenu.classList.add("hidden");
             burgerMenuLinkContainer.classList.toggle("hidden");
         });
     }
